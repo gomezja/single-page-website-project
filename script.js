349690a8d8833fb34451194bb0ebf8bbb4f3dd2e
gomezja <jgomez42gb@gmail.com>
2019-01-22 13:39:14 -0800

    Added descriptor for project cells

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// hamburger icon is pressed
 $(document).ready(function() {
+    // hamburger icon is clicked
     $("#ham").click(function() {
         $("#content").toggleClass("nav__content--responsive");
         $("#ham").toggleClass("nav__icon--transform-x");
     });
 
+    // project cell is clicked
     $(".project-section__cell").click(function() {
         $("#close-up").css("display", "block");
         $("#close-up__image").attr("src", $(this).find("img").attr("src"));
     });
 
+    // project close up area is clicked
     $("#close-up").click(function() {
         $(this).css("display", "none");
         $("#close-up__image").attr("src", "");
