52335025f52084920bdab550906a19234ab76638
gomezja <jgomez42gb@gmail.com>
2019-01-22 10:29:21 -0800

    Added expand feature for project images

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ $(document).ready(function() {
         $("#content").toggleClass("nav__content--responsive");
         $("#ham").toggleClass("nav__icon--transform-x");
     });
+
+    $(".project-section__cell").click(function() {
+        $("#close-up").css("display", "block");
+        $("#close-up__image").attr("src", $(this).find("img").attr("src"));
+    });
+
+    $("#close-up").click(function() {
+        $(this).css("display", "none");
+        $("#close-up__image").attr("src", "");
+    });
 });
