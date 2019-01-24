e8d32aafdcff2fff6df0e742cf5b8f7c19e3d5f7
gomezja <jgomez42gb@gmail.com>
2019-01-23 17:49:06 -0800

    Added header for expanded project images

--- script.js
+++ script.js
@@ -9,11 +9,28 @@ $(document).ready(function() {
     $(".project-section__cell").click(function() {
         $("#close-up").css("display", "block");
         $("#close-up__image").attr("src", $(this).find("img").attr("src"));
+
+        // change text content in close-up
+        $(".close-up__name").text($(this).find(".project-section__project-name").text());
+        $(".close-up__type").text($(this).find(".project-section__project-type").text());
+
+        // add close-up type class
+        if($(this).find(".project-section__project-type").text().trim() == 'Design'){
+            $(".close-up__type").addClass("project-section__project-type--design");
+        } else {
+            $(".close-up__type").addClass("project-section__project-type--development");
+        }
     });
 
     // project close up area is clicked
-    $("#close-up").click(function() {
-        $(this).css("display", "none");
+    $(".close-up__button").click(function() {
+        $("#close-up").css("display", "none");
         $("#close-up__image").attr("src", "");
+
+        // remove close-up type class
+        if($(".close-up__type").hasClass("project-section__project-type--design"))
+            $(".close-up__type").removeClass("project-section__project-type--design");
+        else
+            $(".close-up__type").removeClass("project-section__project-type--development");
     });
 });
